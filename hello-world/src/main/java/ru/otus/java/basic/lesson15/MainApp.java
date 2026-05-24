e92d88b09812d2f8c115a1881ae4f63b987e2deb
Anat382 <62346931+Anat382@users.noreply.github.com>
2026-05-24 17:45:31 +0700

    lesson 15 v2

--- hello-world/src/main/java/ru/otus/java/basic/lesson15/MainApp.java
+++ hello-world/src/main/java/ru/otus/java/basic/lesson15/MainApp.java
@@ -20,7 +20,7 @@ public class MainApp {
 
         String[][] arrIntError = {
                 {"1", "2", "3", "4"},
-                {"5", "6", "7d", "8"},
+                {"5", "6", "7s", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
@@ -31,16 +31,16 @@ public class MainApp {
                 arrIntError
         };
 
-            int number = 0;
-            for (Object arr : arrais) {
-                try {
-                    System.out.println("\nМассив: " + number);
-                    System.out.println("Сумма значений массива: " + sumElementsArray((String[][]) arr));
-                } catch (AppArraySizeException | AppArrayDataException e) {
-                    e.printStackTrace();
-                }
-                number++;
+        int number = 0;
+        for (Object arr : arrais) {
+            try {
+                System.out.println("\nМассив: " + number);
+                System.out.println("Сумма значений массива: " + sumElementsArray((String[][]) arr));
+            } catch (AppArraySizeException | AppArrayDataException e) {
+                e.printStackTrace();
             }
+            number++;
+        }
 
         System.out.println("Конец");
 
@@ -49,15 +49,14 @@ public class MainApp {
     public static int sumElementsArray(String[][] array) {
         int result = 0;
         for (int l1 = 0; l1 < array.length; l1++) {
-            int sizeArr = array.length * array[l1].length;
-            if (sizeArr != 16) {
+            if (array.length != 4 || array[l1].length != 4) {
                 throw new AppArraySizeException("\nНекорректный размер массива: ", "count row: " + array.length + ", length row: " + array[l1].length);
             } else {
                 for (int l2 = 0; l2 < array[l1].length; l2++) {
-                    if (array[l1][l2] == null || !array[l1][l2].matches("\\d+")) {
-                        throw new AppArrayDataException("Некорректный тип данных, не удалось преобразовать текст в int: ", "value: " + array[l1][l2] + ", index row: " + l1 + ", index value: " + l2 );
-                    } else {
+                    try {
                         result += Integer.parseInt(array[l1][l2]);
+                    } catch (NumberFormatException e) {
+                        throw new AppArrayDataException("\nНекорректный тип данных, не удалось преобразовать текст в int: ", "value: " + array[l1][l2] + ", index row: " + l1 + ", index value: " + l2);
                     }
                 }
             }
@@ -65,3 +64,6 @@ public class MainApp {
         return result;
     }
 }
+
+//array[l1][l2].matches("\\d+"
+//
